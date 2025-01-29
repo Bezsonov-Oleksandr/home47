12b25f0eae4d4d90f60bff3d219ae0d312944ce9
Bezsonov-Oleksandr <sascha.bezsonov@gmail.com>
2025-01-29 21:32:50 +0100

    Add new classes

---
+++ src/main/java/home_work/Customer.java
@@ -0,0 +1,10 @@
+package home_work;
+
+public class Customer {
+    private int id;
+    private String name;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/home_work/Product.java
@@ -0,0 +1,12 @@
+package home_work;
+
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
